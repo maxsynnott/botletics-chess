2d04773fc461db9e16e384d077e0e541bbd7873c
maxsynnott <maxryansynnott@gmail.com>
2020-12-27 02:44:01 +0100

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 import express from "express";
+import cors from "cors";
+
 import routes from "./routes";
 
 createConnection()
 	.then(() => {
 		const app = express();
+
+		app.use(cors());
 
 		Object.entries(routes).forEach(([path, router]) => app.use(path, router));
 
